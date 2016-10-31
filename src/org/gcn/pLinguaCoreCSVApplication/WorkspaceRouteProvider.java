627b71a8a07ac63fbfbc23d66d7524cb0985ac9f
manulitogarcia <mgarcia.universidad@gmail.com>
2016-10-31 11:22:55 -0400

    Renamed main package to MeCoGUI

--- src/org/gcn/pLinguaCoreCSVApplication/WorkspaceRouteProvider.java
+++
@@ -1,8 +0,0 @@
-package org.gcn.pLinguaCoreCSVApplication;
-
-public class WorkspaceRouteProvider {
-	public String getWorkspaceRoute(){
-		return System.getenv().get("WORKSPACE").replace('\\', '/');
-	}
-
-}
